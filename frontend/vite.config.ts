f661e1d72f700fc28d12b29ba46ee9ca70584871
Yoki Jati Perkasa <112103702+yokijatip@users.noreply.github.com>
2025-07-20 23:15:54 +0700

    Fix Tailwind CSS Installation and Configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
@@ -18,5 +15,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000
+  },
+  css: {
+    postcss: './postcss.config.js',
   }
 })
