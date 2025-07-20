14adccaf5ac3530619b53b06bff64d501f64fbb1
Yoki Jati Perkasa <112103702+yokijatip@users.noreply.github.com>
2025-07-20 23:14:18 +0700

    Fix Tailwind PostCSS Configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
